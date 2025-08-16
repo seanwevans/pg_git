fb586d89d9b015de939b9706f1d08323183e8b04
Sean Evans <sean.w.evans@gmail.com>
2025-08-15 20:35:55 -0400

    Update pgit-control.sql

--- sql/pgit-control.sql
+++ sql/pgit-control.sql
@@ -1,7 +1,7 @@
 comment = 'Git implementation in PostgreSQL'
 default_version = '0.4.0'
 module_pathname = '$libdir/pg_git'
-relocatable = true
+relocatable = false
 schema = pg_git
 requires = 'plpgsql'
 superuser = false
